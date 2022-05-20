27dd600f5e619f926b4c7e4740dd219c63c76196
Jesus Enríquez Jaime <jesus-e98@hotmail.com>
2022-05-19 23:19:12 -0700

    :sparkles: Now you can generate income sheet

--- controller/generateIncomeStatement/index.js
+++ controller/generateIncomeStatement/index.js
@@ -1,50 +1,5 @@
 const enterpriseModel = require("./../../models/Enterprise");
-
-function sumMovements(movements){
-  let sum = 0;
-  for(let movement of movements){
-    if (movement.charge) sum -= movement.charge;
-    else if (movement.deposit) sum += movement.deposit;
-  }
-  return sum;
-}
-
-function generateDataSet(initialDate, endDate, accounts){
-  let dataSet = [];
-  let father = -1;
-  for(let account of accounts){
-    console.log(account);
-
-    if(account.level !== 1){
-      if(account.affectable && account.movements.length > 0){
-        let periodSum = sumMovements(account.movements);
-
-        let stateAccount = {name: account.nameAccount, periodSum, accumulatedSum: periodSum + account.initialBalance}
-
-        if(account.level === 3){
-          console.log(dataSet);
-          dataSet[father].subAccounts = [... dataSet[father].subAccounts, stateAccount] ;
-
-          dataSet[father].periodTotal += periodSum;
-          dataSet[father].accumulatedTotal += periodSum;
-        } else {
-          dataSet = [... dataSet, stateAccount];
-          father++;
-        }
-
-      } else if(!account.affectable) { 
-        let fatherAccount = {name: account.nameAccount, periodTotal: 0, accumulatedTotal: account.initialBalance || 0, subAccounts: []}
-        dataSet = [... dataSet, fatherAccount];
-
-        father++;
-      }
-    }
-  }
-
-  console.log(dataSet[0]);
-
-  return dataSet;
-}
+const { generatePercentages, generateSums } = require("./movementAdder");
 
 async function generateIncomeStatement(initialDate, endDate, nameEnterprise, pdf){
   const enterprise = await enterpriseModel.findOne({nameEnterprise}).exec();
@@ -55,13 +10,23 @@ async function generateIncomeStatement(initialDate, endDate, nameEnterprise, pdf
     const income = accounts.filter(account => account.type[0] === "H");
     const outcome = accounts.filter(account => account.type[0] === "G");
 
-    //let incomeSums = generateDataSet(initialDate, endDate, income);
-    let outcomeSums = generateDataSet(initialDate, endDate, outcome);
+    let incomeSums = generateSums(initialDate, endDate, income);
+    let outcomeSums = generateSums(initialDate, endDate, outcome);
 
-    //console.log(incomeSums);
-    console.log(outcomeSums);
+    const incomeTotalPeriod = incomeSums.periodTotal;
+    const incommeTotalAccumulated = incomeSums.accumulatedTotal;;
 
-    return {outcomeSums};
+    incomeSums = generatePercentages(incomeSums, incomeTotalPeriod, incommeTotalAccumulated);
+    outcomeSums = generatePercentages(outcomeSums, incomeTotalPeriod, incommeTotalAccumulated);
+
+    profit = {
+      period: incomeTotalPeriod - outcomeSums.periodTotal,
+      periodPercentage: incomeSums.periodPercentage - outcomeSums.periodPercentage,
+      accumulated: incommeTotalAccumulated - outcomeSums.accumulatedTotal,
+      accumulatedPercentage: incomeSums.accumulatedPercentage - outcomeSums.accumulatedPercentage
+    }
+
+    return {income: incomeSums, outcome: outcomeSums, profit};
   } else {
     console.log("Not found");
     return null;
