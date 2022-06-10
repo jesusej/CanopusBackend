6f9a2d622b86e589fdb1176244460aaab019f2af
Mario Andrés Acevedo Pico <acevedopico@hotmail.com>
2022-06-09 22:02:43 -0600

    router generate balance

--- routes/v1/reports.js
+++ routes/v1/reports.js
@@ -2,12 +2,21 @@ const express = require("express");
 
 const generateIncomeStatement = require("./../../controller/generateIncomeStatement");
 
+const generateTrialBalance  = require("../../controller/geenrateTrialBalance");
+
 let router = express.Router();
 
 router.get("/generate-income-statement/:initialDate/:endDate/:user/:nameEnterprise/:pdf", async(req, res) => {
 
   let data = await generateIncomeStatement(req.params.initialDate, req.params.endDate, req.params.nameEnterprise, req.params.pdf);
   
+  res.status(200).send(data);
+});
+
+router.get("/trial-balance/:nameEneterprise", async(req, res) => {
+
+  let data = await generateTrialBalance(req.params.nameEneterprise);
+
   res.status(200).send(data);
 });
 
